6b112e26af83a5fb5bb9c171c0006399d61afb4b
wits <wits9747@gmail.com>
2024-06-27 18:53:51 +0900

    몰라모라

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,10 @@ import Join from './pages/Join'
 import User from './pages/User'
 import About from './pages/About'
 import Login from './pages/Login'
-<<<<<<< HEAD
+import Insert from './pages/board/Insert';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import InsertForm from './components/board/InsertForm';
-=======
-import Insert from './pages/board/Insert';
 
->>>>>>> 7903d3f252679d157c290af50c6882f92d8ebb0c
 
 function App() {
   return (
@@ -25,12 +21,7 @@ function App() {
           <Route path="/Join" element={<Join/>}></Route>
           <Route path="/User" element={<User/>}></Route>
           <Route path="/About" element={<About/>}></Route>
-<<<<<<< HEAD
-          <Route path="/inset" element={<InsertForm/>}></Route>
-          
-=======
           <Route path="/board/Insert" element={<Insert/>}></Route>
->>>>>>> 7903d3f252679d157c290af50c6882f92d8ebb0c
         </Routes>
       </LoginContextProvider>
     </BrowserRouter>
