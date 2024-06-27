1c43a62bdf5585f6affe7285aadb110829a77852
JunSu0191 <tlswnstn0191@naver.com>
2024-06-27 15:37:33 +0900

    수정

---
+++ client/src/components/mypage/PaymentForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentForm = () => {
+  return (
+    <div>Payment</div>
+  )
+}
+
+export default PaymentForm
---
+++ client/src/containers/ProfileContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from '../components/mypage/ProfileForm'
+
+const ProfileContainer = () => {
+    const navigate = useNavigate()
+
+  return (
+    <>
+        <Profile />
+    </>
+  )
+}
+
+export default ProfileContainer
