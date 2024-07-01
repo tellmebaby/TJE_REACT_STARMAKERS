b8cd3cd879fbb4fcf3d4056bcf035fd03aff5eb5
wits <wits9747@gmail.com>
2024-07-01 12:09:21 +0900

    회원가입 완료

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { useNavigate } from 'react-router-dom'
 
 const MainLayout = ({ children }) => {
+  // 페이지 이동
+  const navigate = useNavigate()
   return (
     <>
         <Header/>
             <div className="container">
-             {children} 
+              {React.cloneElement(children, { navigate })} {/* navigate를 props로 전달 */}
             </div>
         <Footer/>
         
