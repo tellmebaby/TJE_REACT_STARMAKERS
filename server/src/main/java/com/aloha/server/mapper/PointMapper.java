3e672a75fadce7e60c3dbd2d3d33dde147ec2895
Joyeeeeeeeeeee <s0236sx@gmail.com>
2024-07-05 11:22:24 +0900

    mapper수정

--- server/src/main/java/com/aloha/server/mapper/PointMapper.java
+++ server/src/main/java/com/aloha/server/mapper/PointMapper.java
@@ -14,11 +14,11 @@ public interface PointMapper {
    // 게시글 조회
    public Files select(int no) throws Exception;
    // 게시글 등록
-   public int insert(Files file) throws Exception;
-   // 게시글 수정
-   public int update(Files file) throws Exception;
-   // 게시글 삭제
-   public int delete(int no) throws Exception;
-   // 조회수 업데이트
-   public int updateViews(@Param("count") int count, @Param("no") int no) throws Exception;
+//    public int insert(Files file) throws Exception;
+//    // 게시글 수정
+//    public int update(Files file) throws Exception;
+//    // 게시글 삭제
+//    public int delete(int no) throws Exception;
+//    // 조회수 업데이트
+//    public int updateViews(@Param("count") int count, @Param("no") int no) throws Exception;
 }
