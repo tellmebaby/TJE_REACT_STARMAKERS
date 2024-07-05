1facfd6759a137c3271fe24c302d82318b079f39
Joyeeeeeeeeeee <s0236sx@gmail.com>
2024-07-05 11:38:39 +0900

    PointMapper.xml까지 완료

--- server/src/main/java/com/aloha/server/dto/point.java
+++ server/src/main/java/com/aloha/server/dto/point.java
@@ -2,10 +2,12 @@ package com.aloha.server.dto;
 
 import java.util.Date;
 
-public class point {
+public class Point {
     private int pointNo;
     private int point;
     private Date regDate;
     private int starNo;
     private String type;
+
+    private int totalPoint;
 }
